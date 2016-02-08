d0fe07ff256f47dc9105ba6c81c89378761fbcdd
Xiaoliang Wu <zurielibra@gmail.com>
2016-02-08 13:47:25 -0600

    implement Natural Join

---
+++ Natural_Join.py
@@ -0,0 +1,23 @@
+file1_obj = open('Natural_Join_file1', 'r')
+
+while 1:
+    file1_line = file1_obj.readline()
+    if len(file1_line) == 0:
+        break
+    file1_line = file1_line[:len(file1_line) - 1]
+    file1_line = file1_line.split(' ')
+
+    file2_obj = open('Natural_Join_file2', 'r')
+    while 1:
+        file2_line = file2_obj.readline()
+        if len(file2_line) == 0:
+            file2_obj.close()
+            break
+        file2_line = file2_line[:len(file2_line) - 1]
+        file2_line = file2_line.split(' ')
+        if file1_line[0] == file2_line[1] and file1_line[2] == file2_line[2]:
+            file1_line.append(file2_line[0])
+            file1_line.append(file2_line[3])
+            print file1_line
+
+
